ccd6ed4e0e2cec81350b9c65fd397ebc2d77c3e9
Rob W <robpwatkins@gmail.com>
2020-05-12 10:30:39 -0500

    a lil stuck

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+    
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;    
+  // }
+};
 
 module.exports = Mutations;
