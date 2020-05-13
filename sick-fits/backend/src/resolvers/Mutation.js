8616297df06ae1814620af85d87cd1190b1afd4a
Rob W <robpwatkins@gmail.com>
2020-05-13 11:30:57 -0500

    yep

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,13 +1,18 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    const item = await ctx.db.mutation.createItem({
-      data: {
-        ...args
-      }
-    }, info);
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      }, 
+      info
+    );
     
+    console.log(item);
+
     return item;
-  }
+  },
   // createDog(parent, args, ctx, info) {
   //   global.dogs = global.dogs || [];
   //   const newDog = { name: args.name };
